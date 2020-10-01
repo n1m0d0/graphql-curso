faa3f12572b14ef8227c323963e4a08134bea441
Johnny Henry Vasco Calle <n1m0d0>
2020-09-30 20:26:05 -0400

    mutation

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { GraphQLServer} from 'graphql-yoga'
+import Query from './resolvers/Query'
+import Author from './resolvers/Author'
+import Book from './resolvers/Book'
+import Mutation from './resolvers/Mutation'
+import db from './db'
+
+const resolvers = {
+    Query,
+    Author,
+    Book,
+    Mutation,
+}
+
+const context = {
+    db
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context
+})
+
+server.start(() => console.log('server is running on localhost:4000'))
